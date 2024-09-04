421931c86503166d56622a2555d33ab43de34acd
MOHAMED FAZRIN <mfmfazrin1986@gmail.com>
2024-09-04 21:53:28 +0300

    feat: Update site and base in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,8 @@ import starlight from "@astrojs/starlight";
 // https://astro.build/config
 export default defineConfig({
   // https://docs.astro.build/en/guides/images/#authorizing-remote-images
-  site: "https://screwfast.uk",
+  site: "https://nirzaf.github.io/quadrate-astro",
+  base: "quadrate-astro",
   image: {
     domains: ["images.unsplash.com"],
   },
