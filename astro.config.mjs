eb744406bb195ce99e64444b95cc230cd806f708
MOHAMED FAZRIN <mfmfazrin1986@gmail.com>
2024-09-04 23:24:30 +0300

    feat: add french locale support

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,14 +7,10 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-  // https://docs.astro.build/en/guides/images/#authorizing-remote-images
   site: "https://nirzaf.github.io/quadrate-astro",
-  image: {
-    domains: ["images.unsplash.com"],
-  },
   i18n: {
     defaultLocale: "en",
-    locales: ["en"],
+    locales: ["en", "fr"],
     routing: {
       prefixDefaultLocale: false,
     },
@@ -26,7 +22,7 @@ export default defineConfig({
       i18n: {
         defaultLocale: "en", // All urls that don't contain `fr` after `https://screwfast.uk/` will be treated as default locale, i.e. `en`
         locales: {
-          en: "en" // The `defaultLocale` value must present in `locales` keys
+          en: "en" // The `defaultLocale` value must present in `locales` key
         },
       },
     }),
